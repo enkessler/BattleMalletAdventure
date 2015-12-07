34b3e291ccbfb7978ba428dc90283a2256610389
Eric Kessler <morrow748@gmail.com>
2015-12-06 22:22:41 -0500

    Redesigned how rooms are modeled and drawn.

---
+++ dungeon_rooms.js
@@ -0,0 +1,56 @@
+function RoomNode(room_styles) {
+	this.style_classes = room_styles;
+}
+
+function TombRoom() {
+	this.room_cells = [ [ 'd', 'd', 'd', 'd' ], [ 'd', 'd', 'd', 'd' ],
+			[ 'd', 'd', 'd', 'd' ], [ 'd', 'd', 'd', 'd' ],
+			[ 'd', 'd', 'd', 'd' ], [ 'd', 'd', 'd', 'd' ],
+			[ 'd', 'd', 'd', 'd' ], [ 'd', 'd', 'd', 'd' ] ];
+}
+
+function StairsRoom() {
+	this.room_cells = [ [ 'd', 'd', 'd', 'd', 'd', 'd' ],
+			[ 'd', 'd', 'd', 'd', 'd', 'd' ] ];
+}
+
+function CorridorRoom() {
+	this.style_class = "corridor_room";
+
+	this.room_nodes = [
+			new RoomNode([ 'corridor_node', 'corridor_node_row_1',
+					'corridor_node_col_1' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_1',
+					'corridor_node_col_2' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_1',
+					'corridor_node_col_3' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_1',
+					'corridor_node_col_4' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_1',
+					'corridor_node_col_5' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_1',
+					'corridor_node_col_6' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_2',
+					'corridor_node_col_1' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_2',
+					'corridor_node_col_2' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_2',
+					'corridor_node_col_3' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_2',
+					'corridor_node_col_4' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_2',
+					'corridor_node_col_5' ]),
+			new RoomNode([ 'corridor_node', 'corridor_node_row_2',
+					'corridor_node_col_6' ]) ];
+}
+
+function LCorridorRoom() {
+	this.room_cells = [ [ 'd', 'd', 'd', 'd' ], [ 'd', 'd', 'd', 'd' ],
+			[ 'd', 'd', ' ', ' ' ], [ 'd', 'd', ' ', ' ' ] ];
+}
+
+function TCorridorRoom() {
+	this.room_cells = [ [ 'd', 'd', 'd', 'd', 'd', 'd' ],
+			[ 'd', 'd', 'd', 'd', 'd', 'd' ], [ ' ', ' ', 'd', 'd', ' ', ' ' ],
+			[ ' ', ' ', 'd', 'd', ' ', ' ' ] ];
+}
--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-var map_cells = [ [] ]
+var map_rooms = [ new CorridorRoom(10, 10) ];
 
 $(document).ready(function() {
+
+	// Display the initial map
+	rebuild_map();
 
 	// Configure room button
 	$('#spawn_room_button').click(function() {
@@ -11,31 +14,51 @@ $(document).ready(function() {
 });
 
 function add_room_to_map() {
+	console.log('Adding room to map');
 
-	map_cells = [ [ 1, 2 ], [ 1, 2, 3, 4 ] ];
-
+	map_rooms.push(new CorridorRoom(100, 100));
 };
 
 function rebuild_map() {
-
 	var map = document.getElementById('dungeon_map');
+
+//	console.log('First Child: ' + map.firstChild);
 
 	while (map.firstChild) {
 		map.removeChild(map.firstChild)
 	}
 
-	for (i = 0; i < map_cells.length; i++) {
-		console.log('Adding row ' + i);
-		var table_row = document.createElement('tr');
+	for (i = 0; i < map_rooms.length; i++) {
+		console.log('Building room ' + i);
+		console.log('room class: ' + map_rooms[i].style_class);
 
-		for (j = 0; j < map_cells[i].length; j++) {
-			console.log('Adding column ' + i);
-			var table_cell = document.createElement('td');
-			table_cell.appendChild(document.createTextNode('a'));
-			table_row.appendChild(table_cell);
+		var room_div = document.createElement('div');
+		var map_room = map_rooms[i];
+
+		room_div.className = map_room.style_class;
+
+		for (j = 0; j < map_room.room_nodes.length; j++) {
+			console.log('Building room node ' + j);
+			
+			
+			var node_div = document.createElement('div');
+			var room_node = map_room.room_nodes[j];
+
+			console.log('node classes (' + room_node.style_classes.length + '): ' + room_node.style_classes);
+
+			for (k = 0; k < room_node.style_classes.length; k++) {
+				console.log('Adding class: ' + room_node.style_classes[k]);
+				node_div.className += ' ' + room_node.style_classes[k];
+			}
+
+
+			room_div.appendChild(node_div);
 		}
-		map.appendChild(table_row);
+
+		map.appendChild(room_div);
 	}
 
+	console.log('Build complete');
+//	console.log('First Child: ' + map.firstChild);
 };
 
